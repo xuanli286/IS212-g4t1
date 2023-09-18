5fecb892cd2abcc41a5ecc98050119de3e5bbdce
xuanli286 <xuanli743@gmail.com>
2023-09-18 13:49:08 +0800

    IG-8 add modal for edit role listing

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,15 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div>
+        <div id="modal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-10 backdrop-blur-sm">
+            <div class="bg-white shadow-lg rounded-lg p-6">
+                <div class="flex items-center">
+                    hi
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
