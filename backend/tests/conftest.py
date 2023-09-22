01dd4ad4257f4c729439cc4f0e7db6b3481a4c50
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2023-09-22 15:04:01 +0800

    IG-6 updated pytest fixture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,22 +4,20 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from g4t1 import app, db
+from g4t1_test import app, db
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    client = app.test_client()
-    
+
     # Create the test database
     with app.app_context():
-        db.create_all() 
-        print("Test database created successfully.")
+        db.create_all()
 
+    # Create a test client for the application
     with app.test_client() as client:
         yield client
-    
-    # Drop the test database
+
+    # Drop the test database after the tests are finished
     with app.app_context():
-        db.drop_all() 
-        print("Test database dropped successfully.")
+        db.drop_all()
--- backend/tests/test_IG_6.py
+++ backend/tests/test_IG_6.py
@@ -1,6 +1,6 @@
 import json
 import pytest
-from g4t1 import RoleListing
+from g4t1_test import RoleListing
 from conftest import client, app
 
 def test_create_rolelisting(client):
@@ -26,13 +26,14 @@ def test_create_rolelisting(client):
     with app.app_context():
         created_role_listing = RoleListing.query.filter(
             RoleListing.role_name == "Software Engineer",
-            RoleListing.start_date == "2023-09-21",
-            RoleListing.end_date == "2023-12-31"
+            RoleListing.application_opening == "2023-09-21",
+            RoleListing.application_deadline == "2023-12-31"
         ).first()
         assert created_role_listing is not None
         
         
 def test_duplicate_rolelisting(client):
+    
     rolelisting_data = {
         "role_name": "Software Engineer",
         "application_opening": "2023-09-15",
@@ -41,15 +42,17 @@ def test_duplicate_rolelisting(client):
         "country": "Hong Kong",
         "manager_ID": "1033",
     }
-
+    
     response = client.post("/addrolelisting", json=rolelisting_data)
 
-    assert response.status_code == 400
+    response_duplicate = client.post("/addrolelisting", json=rolelisting_data)
 
-    response_data = json.loads(response.data)
+    assert response_duplicate.status_code == 400
 
-    assert "code" in response_data
-    assert "data" in response_data
-    assert "message" in response_data
-    assert response_data["message"] == 'Role Listing exists.'
+    response_duplicate_data = json.loads(response_duplicate.data)
+
+    assert "code" in response_duplicate_data
+    assert "data" in response_duplicate_data
+    assert "message" in response_duplicate_data
+    assert response_duplicate_data["message"] == 'Role Listing exists.'
 
