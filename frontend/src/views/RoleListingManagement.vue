38d7876a8548e3877e21fed7ec6b3e77a31e1cb8
Faez <faezlatiff0706@gmail.com>
2023-09-26 00:39:46 +0800

    IG-5 Invoke axios call and clean data

--- frontend/src/views/RoleListingManagement.vue
+++ frontend/src/views/RoleListingManagement.vue
@@ -1,10 +1,35 @@
+<script setup>
+  import axios from "axios";
+
+  var roleListings = []
+  axios
+    .get("http://127.0.0.1:5000/openrolelisting")
+    .then((response) => {
+      roleListings = cleanResponseData(response)
+      console.log(roleListings);
+
+    })
+
+  function cleanResponseData(response){
+    roleListings = response.data.data.rolelisting
+    var cleanedRoleListings = []
+      for(let i = 0; i < roleListings.length; i++){
+        let key = Object.keys(roleListings[i])
+        let roleListingObj = roleListings[i][key]
+        cleanedRoleListings.push(roleListingObj)
+      }
+    return cleanedRoleListings
+  }
+
+</script>
+
 <template>
   <div>
     <h1>Role Listing Management</h1>
   </div>
 </template>
 
-<script>
+<!-- <script>
 export default {
   name: 'RoleListingManagement',
   components: {},
@@ -19,6 +44,6 @@ export default {
   created() {
   },
 }
-</script>
+</script> -->
 
 <style></style>
