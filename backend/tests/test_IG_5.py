3f2da1751fb1b3e0aabd3f8e086e4f4fd44da229
Faez <faezlatiff0706@gmail.com>
2023-09-25 23:29:56 +0800

    IG-5 Added test file and webdriver

---
+++ backend/tests/test_IG_5.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def chrome_driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+@pytest.mark.parametrize("url, element_selector", [
+    ("http://localhost:8080/allrolelisting", "#rolelistings"),
+    # Add more test cases as needed
+])
+def test_check_rolelistings_ui(chrome_driver, url, element_selector):
+    driver = chrome_driver
+    
+    driver.get(url)
+    element = driver.find_element(By.CSS_SELECTOR, element_selector)
+    
+    assert element.is_displayed()
