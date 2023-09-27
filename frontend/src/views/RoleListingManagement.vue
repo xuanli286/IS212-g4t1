58a43bb9d84b5a350b553121c7798247261efaa2
Faez <faezlatiff0706@gmail.com>
2023-09-27 14:43:49 +0800

    IG-5 Dynamically show listings

--- frontend/src/views/RoleListingManagement.vue
+++ frontend/src/views/RoleListingManagement.vue
@@ -37,12 +37,12 @@
       </div>
     </div>
     <ul class="mx-64 min-w-fit">
-      <li class="flex border-t py-5 hover:bg-grey-50">
+      <li v-for="(listing, id) in roleListings" :key="id" class="flex border-t py-5 hover:bg-grey-50">
         <div class="flex-none h-100">
-          <div class="text-yellow text-xl"> Software Engineer </div>
-          <div class="text-base"> Reporting Manager: 1003 </div>
+          <div class="text-yellow text-xl"> {{listing.role_name}} </div>
+          <div class="text-base"> Reporting Manager: {{listing.manager_ID}} </div>
           <div class="flex flex-row text-xs"> 
-            <div class="text-grey"> Apply by 5 September 2023 </div>
+            <div class="text-grey"> Apply by {{listing.application_deadline}}</div>
             <div class="flex items-center mx-2">
               <span class="bg-black h-1 w-1 rounded-full"></span>
             </div>
@@ -53,65 +53,16 @@
         <div>
           <div class="flex flex-row">
             <div class="grow"></div>
-            <div class="me-5 text-xs bg-red px-4 py-1 rounded-full text-white">Closed</div>
-          </div>
-          <div class="flex flex-row items-center text-yellow pt-3">
-            <div class="font-bold">Engineering Operation</div>
-            <div class="flex bg-yellow mx-2 h-1 w-1 rounded-full"> </div>
-            <div>Hong Kong</div>
-          </div>
-        </div>
-      </li>
-      <li class="flex border-t py-5 hover:bg-grey-50">
-        <div class="flex-none h-100">
-          <div class="text-yellow text-xl"> Software Engineer </div>
-          <div class="text-base"> Reporting Manager: 1003 </div>
-          <div class="flex flex-row text-xs"> 
-            <div class="text-grey"> Apply by 5 September 2023 </div>
-            <div class="flex items-center mx-2">
-              <span class="bg-black h-1 w-1 rounded-full"></span>
-            </div>
-            <div class="font-bold text-green"> 11 applicants </div>
-          </div>
-        </div>
-        <div class="grow"></div>
-        <div>
-          <div class="flex flex-row ">
-            <div class="grow"></div>
             <div class="me-5 text-xs bg-green px-4 py-1 rounded-full text-white">Open</div>
           </div>
           <div class="flex flex-row items-center text-yellow pt-3">
-            <div class="font-bold">Engineering Operation</div>
+            <div class="font-bold">{{ listing.dept }}</div>
             <div class="flex bg-yellow mx-2 h-1 w-1 rounded-full"> </div>
-            <div>Hong Kong</div>
+            <div>{{ listing.country }}</div>
           </div>
         </div>
       </li>
-      <li class="flex border-t py-5 hover:bg-grey-50">
-        <div class="flex-none h-100">
-          <div class="text-yellow text-xl"> Software Engineer </div>
-          <div class="text-base"> Reporting Manager: 1003 </div>
-          <div class="flex flex-row text-xs"> 
-            <div class="text-grey"> Apply by 5 September 2023 </div>
-            <div class="flex items-center mx-2">
-              <span class="bg-black h-1 w-1 rounded-full"></span>
-            </div>
-            <div class="font-bold text-green"> 11 applicants </div>
-          </div>
-        </div>
-        <div class="grow"></div>
-        <div>
-          <div class="flex flex-row ">
-            <div class="grow"></div>
-            <div class="me-5 text-xs bg-green px-4 py-1 rounded-full text-white">Open</div>
-          </div>
-          <div class="flex flex-row items-center text-yellow pt-3">
-            <div class="font-bold">Engineering Operation</div>
-            <div class="flex bg-yellow mx-2 h-1 w-1 rounded-full"> </div>
-            <div>Hong Kong</div>
-          </div>
-        </div>
-      </li>
+ 
     </ul>
   </div>
 </template>
