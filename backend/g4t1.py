67903d05ff952bee4ca214196f9327816f00b461
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2023-09-28 10:36:52 +0800

    IG-28 Updated workflow file to do on PR instead of push

--- backend/g4t1.py
+++ backend/g4t1.py
@@ -499,4 +499,4 @@ def get_all_applications_for_a_rolelisting(rolelisting_ID):
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, host="0.0.0.0", debug=True)
