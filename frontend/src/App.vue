23c6e5ef5db7d38b6ef4d500bf54574f6fa0f813
xuanli286 <xuanli743@gmail.com>
2023-09-29 01:23:29 +0800

    add icon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="min-h-screen bg-beige">
-    <Logout v-if="user" class="px-10 mb-3"/>
-    <router-view class="px-10"></router-view>
-  </div>
+<div class="min-h-screen bg-beige">
+  <Logout v-if="user" class="px-10 mb-3"/>
+  <router-view class="px-10"></router-view>
+</div>
 </template>
 
 <script setup>
-  import Logout from '@/components/Logout.vue'
-  import { useUserStore } from '@/store/useUserStore.js';
-  import { storeToRefs } from 'pinia';
+import Logout from '@/components/Logout.vue'
+import { useUserStore } from '@/store/useUserStore.js';
+import { storeToRefs } from 'pinia';
 
-  const userStore = useUserStore();
-  const {
-    user,
-  } = storeToRefs(userStore);
+const userStore = useUserStore();
+const {
+  user,
+} = storeToRefs(userStore);
 </script>
