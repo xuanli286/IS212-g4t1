583ca52e4b562939e9b396122dc44e5997f60350
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2023-10-08 11:56:25 +0800

    IG-28 test workflow

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 
-backend_base_url = "http://13.229.118.77:5000"        
+backend_base_url = "http://13.212.177.124:5000"        
 frontend_base_url = "http://localhost:8080"
 
 @pytest.fixture
