b6e4dacf2127e98fb63ad124f6b834808ea85f80
Blobsterdude <domtam810@hotmail.com>
2023-10-10 04:43:29 +0800

    IG-14 Updated fields
    Still require access control and update more fields

--- frontend/src/views/SpecificRoleListing.vue
+++ frontend/src/views/SpecificRoleListing.vue
@@ -60,6 +60,9 @@
         <div class="relative w-full max-w-xxl pt-12 h-full md:h-auto">
           <!-- Modal content -->
           <div class="bg-white rounded-lg shadow relative">
+            <div class="bg-yellow w-full">
+              <span class="text-white justify-center">Confirm Details</span>
+            </div>
             <div class="flex justify-end p-2">
               <button
                 type="button"
@@ -84,78 +87,44 @@
               class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8"
               action="#"
             >
-              <h3 class="text-xl font-medium text-gray-900 dark:text-white">
-                Sign in to our platform
-              </h3>
-              <div>
-                <label
-                  for="email"
-                  class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-                  >Your email</label
-                >
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="name@company.com"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="password"
-                  class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-                  >Your password</label
-                >
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  required=""
-                />
-              </div>
-              <div class="flex justify-between">
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      class="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                      required=""
-                    />
-                  </div>
-                  <div class="text-sm ml-3">
-                    <label
-                      for="remember"
-                      class="font-medium text-gray-900 dark:text-gray-300"
-                      >Remember me</label
-                    >
-                  </div>
+              <div class="grid grid-cols-3 pt-5 gap-28">
+                <div>
+                  <p class="font-bold">Hiring Department</p>
+                  <input
+                    type="text"
+                    class="mt-1 w-full p-2 bg-white border-2 border-yellow rounded-md"
+                    v-model="managerID"
+                  />
                 </div>
-                <a
-                  href="#"
-                  class="text-sm text-blue-700 hover:underline dark:text-blue-500"
-                  >Lost Password?</a
-                >
+                <div>
+                  <p class="font-bold">Country</p>
+                  <input
+                    type="text"
+                    class="mt-1 w-full p-2 bg-white border-2 border-yellow rounded-md"
+                    v-model="managerID"
+                  />
+                </div>
+                <div>
+                  <p class="font-bold">Reporting Manager</p>
+                  <input
+                    type="text"
+                    class="mt-1 w-full p-2 bg-white border-2 border-yellow rounded-md"
+                    v-model="managerID"
+                  />
+                </div>
               </div>
               <button
                 type="submit"
-                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="text-white bg-yellow border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded-md text-md mr-3"
               >
-                Login to your account
+                Apply
               </button>
-              <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered?
-                <a
-                  href="#"
-                  class="text-blue-700 hover:underline dark:text-blue-500"
-                  >Create account</a
-                >
-              </div>
+              <button
+                @click="closeApply"
+                class="text-white bg-grey border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded-md text-md"
+              >
+                Cancel
+              </button>
             </form>
           </div>
         </div>
