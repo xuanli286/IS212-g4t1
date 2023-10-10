2261a4cb188fd3840d9fc79c26096c1592ff03db
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2023-10-10 10:48:44 +0800

    IG-28 test workflow

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,39 +1,38 @@
-import pytest
+# import pytest
 from selenium import webdriver
-
-
-backend_base_url = "http://13.212.177.124:5000"        
-frontend_base_url = "http://localhost:8080"
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+import pytest
 
 @pytest.fixture
 def chrome_driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-    # display = Display(visible=0, size=(800, 800))  
-    # display.start()
+    # Set up a virtual display (for headless mode)
+    display = Display(visible=0, size=(800, 800))
+    display.start()
     
-    # chromedriver_autoinstaller.install()
-    # chrome_options = webdriver.ChromeOptions()    
-    # options = [
-    # "--window-size=1200,1200",
-    #     "--ignore-certificate-errors"
+    # Install and configure Chrome WebDriver
+    chromedriver_autoinstaller.install()
+    chrome_options = webdriver.ChromeOptions()
     
-    #     #"--headless",
-    #     #"--disable-gpu",
-    #     #"--window-size=1920,1200",
-    #     #"--ignore-certificate-errors",
-    #     #"--disable-extensions",
-    #     #"--no-sandbox",
-    #     #"--disable-dev-shm-usage",
-    #     #'--remote-debugging-port=9222'
-    # ]
+    options = [
+        "--window-size=1200,1200",
+        "--ignore-certificate-errors",
+        "--headless",  # Enable headless mode
+        "--disable-gpu",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
 
-    # for option in options:
-    #     chrome_options.add_argument(option)
+    for option in options:
+        chrome_options.add_argument(option)
         
-    # driver = webdriver.Chrome(options = chrome_options)
-    return driver
+    # Create and return the WebDriver instance
+    driver = webdriver.Chrome(options=chrome_options)
+    yield driver
+    
+    # Clean up resources by quitting the WebDriver
+    driver.quit()
+
 
 
 # import chromedriver_autoinstaller
