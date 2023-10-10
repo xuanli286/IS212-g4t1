32952cbb69c61c288908001c23b23c0bec5de5a2
desireely <105425207+desireely@users.noreply.github.com>
2023-10-10 14:29:31 +0800

    IG-7 edited api calls>

--- frontend/src/views/RoleListingManagement.vue
+++ frontend/src/views/RoleListingManagement.vue
@@ -1,94 +1,94 @@
 <script setup>
-  import axios from "axios";
-  import { ref } from "vue";  
-  import { useRoleListingStore } from '@/store/useRoleListingStore';
-  import { storeToRefs } from 'pinia';
-  import { useConstantStore } from '@/store/useConstantStore';
+import axios from "axios";
+import { ref } from "vue";
+import { useRoleListingStore } from '@/store/useRoleListingStore';
+import { storeToRefs } from 'pinia';
+import { useConstantStore } from '@/store/useConstantStore';
 
-  const constStore = useConstantStore();
-  const { backend_url } = storeToRefs(constStore);
-  const roleListingStore = useRoleListingStore();
-  const roleListings = ref({})
-  const staffNames = ref({})
+const constStore = useConstantStore();
+const { backend_url } = storeToRefs(constStore);
+const roleListingStore = useRoleListingStore();
+const roleListings = ref({})
+const staffNames = ref({})
 
-  axios
-    .get(`${backend_url.value}/openrolelisting`)
-    .then((response) => {
-      let receivedListings = response.data.data.rolelisting
-      for(let listing of receivedListings){
-        for(let key in listing){
-          roleListings.value[key] = listing[key]
-        }
+axios
+  .get(`${backend_url.value}/openrolelisting`)
+  .then((response) => {
+    let receivedListings = response.data.data.rolelisting
+    for (let listing of receivedListings) {
+      for (let key in listing) {
+        roleListings.value[key] = listing[key]
       }
-    })
-    .catch(error => {
-      if (error.response) {
-        if (error.response.status === 404) {
-          console.log("No open listings at this moment.");
-        }
-      }
-    }) 
-
-  axios
-    .get(`${backend_url.value}/closerolelisting`)
-    .then((response) => {
-      let receivedListings = response.data.data.rolelisting
-      for(let listing of receivedListings){
-        for(let key in listing){
-          roleListings.value[key] = listing[key]
-        }
-      }
-    })
-    .catch(error => {
-      if (error.response) {
-        if (error.response.status === 404) {
-          console.log("No closed listings at this moment.");
-        }
-      }
-    }) 
-
-    axios
-    .get(`${backend_url.value}/staff`)
-    .then((response) => {
-      let allStaff = response.data.data.staff
-      for(let staff of allStaff){
-        for(let key in staff){
-          staffNames.value[key] = staff[key]
-        }
-      }
-    })
-
-  function checkOpen(listing){
-    let today = new Date();
-    let deadline = new Date(listing.application_deadline)
-    if(deadline > today){
-        return true
-      }
-    
-    return false
-  }  
-
-  function formatDate(dateString){
-    let date = new Date(dateString)
-    let day = date.getDate() 
-    let month = date.toLocaleString('default', { month: 'long' });
-    let year = date.getFullYear()
-    return `${day} ${month} ${year}`
-  }
-
-  const updateRoleListingId = (id) => {
-    roleListingStore.setRoleListingId(id)
-  }
-
-  const getManagerName = (id) => {
-    try {
-      var FName = staffNames.value[id]["staff_FName"]
-      var LName = staffNames.value[id]["staff_LName"][0]
-    } catch (error) {
-
     }
-    return `${FName} ${LName}.`
+  })
+  .catch(error => {
+    if (error.response) {
+      if (error.response.status === 404) {
+        console.log("No open listings at this moment.");
+      }
+    }
+  })
+
+axios
+  .get(`${backend_url.value}/closerolelisting`)
+  .then((response) => {
+    let receivedListings = response.data.data.rolelisting
+    for (let listing of receivedListings) {
+      for (let key in listing) {
+        roleListings.value[key] = listing[key]
+      }
+    }
+  })
+  .catch(error => {
+    if (error.response) {
+      if (error.response.status === 404) {
+        console.log("No closed listings at this moment.");
+      }
+    }
+  })
+
+axios
+  .get(`${backend_url.value}/staff`)
+  .then((response) => {
+    let allStaff = response.data.data.staff
+    for (let staff of allStaff) {
+      for (let key in staff) {
+        staffNames.value[key] = staff[key]
+      }
+    }
+  })
+
+function checkOpen(listing) {
+  let today = new Date();
+  let deadline = new Date(listing.application_deadline)
+  if (deadline > today) {
+    return true
   }
+
+  return false
+}
+
+function formatDate(dateString) {
+  let date = new Date(dateString)
+  let day = date.getDate()
+  let month = date.toLocaleString('default', { month: 'long' });
+  let year = date.getFullYear()
+  return `${day} ${month} ${year}`
+}
+
+const updateRoleListingId = (id) => {
+  roleListingStore.setRoleListingId(id)
+}
+
+const getManagerName = (id) => {
+  try {
+    var FName = staffNames.value[id]["staff_FName"]
+    var LName = staffNames.value[id]["staff_LName"][0]
+  } catch (error) {
+
+  }
+  return `${FName} ${LName}.`
+}
 
 </script>
 
@@ -101,11 +101,13 @@
         <div class="grow"></div>
       </div>
       <div class="flex items-center">
-        <router-link id="addRoleListingButton" to="/addrolelisting" class="flex flex-row bg-yellow py-2 px-5 rounded-full text-white text-sm" >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 me-2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <div class="my-auto">Add Role Listing</div>
+        <router-link id="addRoleListingButton" to="/addrolelisting"
+          class="flex flex-row bg-yellow py-2 px-5 rounded-full text-white text-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6 me-2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <div class="my-auto">Add Role Listing</div>
         </router-link>
       </div>
     </div>
@@ -115,13 +117,14 @@
         <div class="font-bold">No listings available!</div>
         <div class="grow"></div>
       </li>
-      <li v-else v-for="(listing, id) in roleListings" :key="id" class="rolelisting-panel flex border-t py-5 hover:bg-grey-50">
-        <router-link to="/specificrolelisting" @click= updateRoleListingId(id)>
+      <li v-else v-for="(listing, id) in roleListings" :key="id"
+        class="rolelisting-panel flex border-t py-5 hover:bg-grey-50">
+        <router-link to="/specificrolelisting" @click=updateRoleListingId(id)>
           <div class="flex-none h-100">
-            <div class="role-title text-yellow text-xl"> {{listing.role_name}} </div>
-            <div class="role-manager text-base"> Reporting Manager: {{getManagerName(listing.manager_ID)}} </div>
+            <div class="role-title text-yellow text-xl"> {{ listing.role_name }} </div>
+            <div class="role-manager text-base"> Reporting Manager: {{ getManagerName(listing.manager_ID) }} </div>
             <div class="flex flex-row text-xs">
-              <div class="role-deadline text-grey"> Apply by {{formatDate(listing.application_deadline)}}</div>
+              <div class="role-deadline text-grey"> Apply by {{ formatDate(listing.application_deadline) }}</div>
               <div class="flex items-center mx-2">
                 <span class="bg-black h-1 w-1 rounded-full"></span>
               </div>
@@ -133,7 +136,8 @@
         <div>
           <div class="flex flex-row">
             <div class="grow"></div>
-            <div :class="{'bg-green': checkOpen(listing), 'bg-red': !checkOpen(listing)}" class="role-status me-5 text-xs px-4 py-1 rounded-full text-white">  
+            <div :class="{ 'bg-green': checkOpen(listing), 'bg-red': !checkOpen(listing) }"
+              class="role-status me-5 text-xs px-4 py-1 rounded-full text-white">
               <span v-if="checkOpen(listing)">Open</span>
               <span v-else>Closed</span>
             </div>
@@ -145,7 +149,7 @@
           </div>
         </div>
       </li>
- 
+
     </ul>
   </div>
 </template>
--- frontend/src/views/SpecificRoleListing.vue
+++ frontend/src/views/SpecificRoleListing.vue
@@ -1,188 +1,125 @@
 <template>
-  <div class="bg-beige px-10 py-5">
-    <div class="flex items-center justify-between">
-      <div class="flex items-center flex-nowrap">
-        <button @click="goBack">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-          </svg>
-        </button>
-        <p class="font-sans pl-4 text-yellow text-2xl">{{ roleName }}</p>
-      </div>
-
-      <!-- Edit button -->
-      <router-link to="/edit-role-listing"
-        class="bg-yellow text-white rounded-full px-4 py-2 flex items-center space-x-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-          class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-        </svg>
-        <span>Edit Role Listing</span>
-      </router-link>
-    </div>
-
-    <div class="flex items-center flex-nowrap pl-8 text-grey">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="w-4 h-4">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>
-      <p class="font-sans pl-2 text-grey text-bold">Expires On:</p>
-      <p class="font-sans pl-2 text-grey pr-8">{{ expiryDate }}</p>
-
-      <!-- display number of applicants -->
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="w-4 h-4 text-green">
-        <path stroke-linecap="round" stroke-linejoin="round"
-          d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-      </svg>
-      <p class="font-sans pl-2 text-green">Number Applicants</p> -->
-
-    </div>
-
-    <div class="mt-8 container mx-auto flex items-center justify-between">
+  <div class="bg-beige px-10 py-7">
+    <div class="flex items-center">
       <div>
-        <p class="text-bold">Reporting Manager</p>
-        <p>{{ managerName }}</p>
+        <div class="flex items-center">
+          <img id="back" src="@/assets/icons/back.svg" alt="" @click=back()>
+          <p class="font-light text-2xl text-yellow ml-4" id="role-name">{{ roleName }}</p>
+        </div>
+        <div class="flex items-center ml-8">
+          <img src="@/assets/icons/clock.svg" alt="">
+          <p id="application-deadline" class="ml-1 text-grey text-sm">
+            <span class="font-bold">Expires On:</span>
+            <span class="font-light ml-1">{{ new Date(roleDetails.application_deadline).toLocaleDateString('en-US', {
+              day:
+                'numeric', month: 'long', year: 'numeric'
+            }) }}</span>
+          </p>
+        </div>
+      </div>
+      <Button id="edit" class="ml-auto" @click="editRole">
+        <template v-slot:icon>
+          <img class="mr-1" src="@/assets/icons/edit.svg" alt="">
+        </template>
+        <template v-slot:text>
+          Edit Role Listing
+        </template>
+      </Button>
+      <Button id="apply" class="ml-auto" @click="applyRole">
+        <template v-slot:icon>
+          <img class="mr-1" src="@/assets/icons/attachdocument.svg" alt="">
+        </template>
+        <template v-slot:text>
+          Apply Role
+        </template>
+      </Button>
+    </div>
+    <div class="grid grid-cols-3 mt-10">
+      <div>
+        <p class="font-bold">Reporting Manager</p>
+        <p id="manager">{{ staff[roleDetails.manager_ID] }}</p>
+      </div>
+      <div class="mx-auto">
+        <p class="font-bold">Hiring Department</p>
+        <p id="hiring-department">{{ roleDetails.dept }}</p>
+      </div>
+      <div class="ml-auto">
+        <p class="font-bold">Country</p>
+        <p id="country">{{ roleDetails.country }}</p>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 gap-20 mt-10">
+      <div>
+        <p class="font-bold">Description</p>
+        <p class="text-justify" id="role-description">{{ roles[roleName] }}</p>
       </div>
       <div>
-        <p class="text-bold">Hiring Department</p>
-        <p>{{ department }}</p>
+        <p class="font-bold">Required Skills</p>
+        <ul id="required-skills" class="list-disc ml-6" v-for="skill of roleSkills" :key="skill">
+          <li>
+            {{ skill }}
+          </li>
+        </ul>
       </div>
-      <div>
-        <p class="text-bold">Country</p>
-        <p>{{ country }}</p>
-      </div>
     </div>
-
-    <div class="mt-16 p-10">
-      <p class=" text-bold">Description</p>
-      <p class="pr-8">{{ description }}</p>
-    </div>
-
-    <div class="mt-1 p-10">
-      <p class="text-bold">Required Skills</p>
-      <ul class="list-disc pl-4">
-        <li v-for="skill in skills" :key="skill">{{ skill }}</li>
-      </ul>
-    </div>
-
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import { useConstantStore } from "@/store/useConstantStore";
+import { useUserStore } from "@/store/useUserStore";
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
 import axios from "axios";
-// import { storeToRefs } from 'pinia';
-import { useConstantStore } from '@/store/useConstantStore';
+import Button from "@/components/Button.vue";
+import router from "@/router";
 
+const route = useRoute();
 
-const rolelistingID = 1
+const userStore = useUserStore();
+const {
+  user,
+} = storeToRefs(userStore);
 
-export default {
-  name: 'SpecificRoleListing',
-  components: {},
-  props: {
-  },
-  setup() {
+const store = useConstantStore();
+const {
+  roles,
+  staff,
+  roleSkills,
+  staffSkills,
+} = storeToRefs(store);
 
-    const backend_url = useConstantStore().backend_url;
+// const rolelistingID = route.params.id;
+const rolelistingID = 2;
+const roleName = ref("");
+const roleDetails = ref({});
 
-    return { backend_url }
-  },
-  data() {
-    return {
-      roleName: "",
-      expiryDate: "",
-      managerId: null,
-      managerName: "",
-      department: "",
-      country: "",
-      skills: [],
-      description: "",
-    };
-  },
-  methods: {
-    getRoleListingInfo() {
-      axios
-        .get(`${this.backend_url}/rolelisting/${rolelistingID}`)
-        .then((response) => {
-          // console.log(response.data.data[rolelistingID])
-          this.roleName = response.data.data[rolelistingID]["role_name"]
-          this.expiryDate = response.data.data[rolelistingID]["application_deadline"]
-          this.managerId = response.data.data[rolelistingID]["manager_ID"]
-          this.department = response.data.data[rolelistingID]["dept"]
-          this.country = response.data.data[rolelistingID]["country"]
-
-          // set managerName variable after obtaining managerId
-          this.getStaffName();
-
-          // set skills variable after obtaining roleName
-          this.getRoleSkills();
-
-          // set description variable after obtaining roleName
-          this.getRoleDescription();
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    },
-    getStaffName() {
-      axios
-        .get(`${this.backend_url}/staff/${this.managerId}`)
-        .then((response) => {
-          const managerData = response.data.data[this.managerId];
-          if (managerData) {
-            this.managerName = managerData["staff_FName"] + " " + managerData["staff_LName"];
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    },
-    getRoleSkills() {
-
-      axios
-        .get(`${this.backend_url}/get_role_skill/${this.roleName}`)
-        .then((response) => {
-          const roleSkillsData = response.data.data;
-          // console.log(roleSkillsData)
-          if (roleSkillsData) {
-            this.skills = roleSkillsData;
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    },
-    getRoleDescription() {
-
-      axios
-        .get(`${this.backend_url}/get_all_role`)
-        .then((response) => {
-          const roleDescriptionsData = response.data.data
-          for (const role of roleDescriptionsData) {
-            for (let rName in role) {
-              if (rName == this.roleName) {
-                this.description = role[this.roleName]
-              }
-            }
-          }
-          // const roleDescription = response.data.data[$roleName];
-          // console.log(roleDescription)
-          // if (roleDescription) {
-          //   this.description = roleDescription;
-          // }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    }
-  },
-  created() {
-    this.getRoleListingInfo();
+onMounted(async () => {
+  try {
+    const response = await axios.get(`http://127.0.0.1:5000/rolelisting/${rolelistingID}`);
+    roleDetails.value = response.data.data[rolelistingID];
+    roleName.value = roleDetails.value.role_name;
+    store.getSkills(roleName.value);
+    store.getStaffSkills(user.value.staff_ID);
   }
-}
-</script>
+  catch (error) {
+    console.log(error.message);
+  }
+})
 
-<style></style>
+function back() {
+  window.history.back();
+}
+
+function editRole() {
+  // TO-DO:
+  router.push({ name: 'editRole' });
+}
+
+function applyRole() {
+  // TO-DO:
+  router.push({ name: 'applyRole' });
+}
+
+</script>
