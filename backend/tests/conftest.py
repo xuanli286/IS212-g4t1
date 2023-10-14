d82d2d22d087984efbfddec495cbca138fea8c9c
Germaine <tanlixuangermaine@gmail.com>
2023-10-14 09:35:28 +0800

    test CD

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 import pytest
 
-backend_base_url = "http://13.212.177.124:5001" # USE FOR POST, PUT & DELETE in test
+backend_base_url = "http://13.212.177.124:5001"
 backend_base_url_production = "http://13.212.177.124:5000" # ONLY USE FOR GET REQUEST
 frontend_base_url = "http://localhost:8080"
 
