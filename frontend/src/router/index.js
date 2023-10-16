560ee0514de0dcf30a6cacee3d6a58fc68af3d95
desireely <105425207+desireely@users.noreply.github.com>
2023-10-17 00:19:44 +0800

    IG-15 created and edited routing for view all applicants page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,7 @@ import SkillManagement from "../views/SkillManagement.vue";
 import StaffViewSpecificRoleListing from "@/views/StaffViewSpecificRoleListing.vue";
 import SpecificRoleListing from "../views/SpecificRoleListing.vue";
 import RoleListingStaff from "../views/RoleListingStaff.vue";
+import ViewAllApplicants from "../views/ViewAllApplicants.vue";
 
 const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
@@ -61,6 +62,11 @@ const router = createRouter({
       path: "/rolelistingstaff",
       name: "Role Listing Staff",
       component: RoleListingStaff,
+    },
+    {
+      path: "/applicants/:id",
+      name: "View All Applicants",
+      component: ViewAllApplicants,
     },
   ],
 });
