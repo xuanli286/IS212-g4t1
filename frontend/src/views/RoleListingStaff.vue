049607453c1863f099c202c82ebb119f16f3c68f
Blobsterdude <domtam810@hotmail.com>
2023-10-24 17:28:37 +0800

    IG-12 Component and features done

--- frontend/src/views/RoleListingStaff.vue
+++ frontend/src/views/RoleListingStaff.vue
@@ -6,6 +6,7 @@ import { useUserStore } from "@/store/useUserStore";
 import router from "@/router";
 import { isSubset } from "@/utils/isSubset";
 import FilterComponent from "@/components/FilterComponent.vue";
+import SearchComponent from "@/components/SearchComponent.vue";
 
 export default {
   data() {
@@ -19,11 +20,12 @@ export default {
       selectedDept: "all",
       selectedSkills: [],
       staffId: {},
-      searchQuery: ""
+      searchQuery: "",
     };
   },
   components: {
     FilterComponent,
+    SearchComponent,
   },
   methods: {
     async fetchData() {
@@ -109,16 +111,43 @@ export default {
       }
       return processedStaff;
     },
-    searchRoles() {
+    async searchRoles(searchQuery) {
       // need to update this so that it will axios call
       var filteredListings = {};
-      for (let listing of Object.keys(this.roleListings)) {
+      const backend_url = useConstantStore().backend_url;
+      var listings = await axios
+        .get(`${backend_url}/openrolelisting`)
+        .then((response) => {
+          return response.data.data.rolelisting;
+        })
+        .catch(() => {
+          return [];
+        });
+
+      for (let listing of listings) {
+        let listingId = Object.keys(listing)[0];
+        var listingSkills = await axios
+          .get(`${backend_url}/get_role_skill/${listing[listingId].role_name}`)
+          .then((response) => {
+            return response.data.data;
+          })
+          .catch(() => {
+            return [];
+          });
+
         if (
-          this.roleListings[listing].role_name
+          listing[listingId].role_name
             .toLowerCase()
-            .includes(this.searchQuery.toLowerCase())
+            .includes(searchQuery.toLowerCase()) ||
+          listing[listingId].dept.toLowerCase() === searchQuery.toLowerCase() ||
+          listing[listingId].country.toLowerCase() ===
+            searchQuery.toLowerCase() ||
+          listingSkills.some(
+            (skill) => skill.toLowerCase() === searchQuery.toLowerCase()
+          )
         ) {
-          filteredListings[listing] = this.roleListings[listing];
+          filteredListings[listingId] = listing[listingId];
+          // console.log(filteredListings, "filtered")
         }
       }
       this.roleListings = filteredListings;
@@ -184,62 +213,11 @@ export default {
       </h1>
     </div>
 
-    <!-- search bar -->
-    <div>
-      <form class="flex items-center pb-10" @submit.prevent="searchRoles">
-        <label for="role-search" class="sr-only">Search</label>
-        <div class="relative w-full">
-          <div
-            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-          >
-            <svg
-              class="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
-              />
-            </svg>
-          </div>
-          <input
-            type="text"
-            id="role-search"
-            v-model="searchQuery"
-            class="bg-gray-50 border border-yellow text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Roles"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          class="p-2.5 ml-2 text-sm font-medium text-blue bg-yellow rounded-lg border border-blue-700 hover:bg-yellow focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"
-        >
-          <svg
-            class="w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-          <span class="sr-only">Search</span>
-        </button>
-      </form>
-    </div>
+    <search-component
+      @search-updated="searchRoles($event)"
+      :search="searchQuery"
+    >
+    </search-component>
 
     <div class="flex flex-row">
       <filter-component
