6a05badfacf62bcf822dc8cb501fe9ed522be545
Blobsterdude <domtam810@hotmail.com>
2023-10-26 12:32:02 +0800

    IG-12 Included bag icon

--- frontend/src/components/SearchComponent.vue
+++ frontend/src/components/SearchComponent.vue
@@ -7,21 +7,7 @@
           <div
             class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
           >
-            <svg
-              class="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
-              />
-            </svg>
+          <img class="w-4 h-4" src="../assets/icons/bag.png" alt="">
           </div>
           <input
             type="text"
